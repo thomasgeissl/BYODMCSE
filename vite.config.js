407acc54e5147a54ffbdb0d6f3151c9ba6758946
Thomas Geissl <thomas.geissl@gmail.com>
2023-05-08 00:31:59 +0200

    fixed base config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  console.log(mode)
 
 
   return defineConfig({
-    base: mode === "dev" ? "/" : "/BYODMCSE/",
+    base: mode === "development" ? "/" : "/BYODMCSE/",
     plugins: [react()],
   });
 }
