bdd1889a42d17133cc8dcb3bb4268fcfd9c38160
Thomas Geissl <thomas.geissl@gmail.com>
2023-05-08 00:18:57 +0200

    trying to fix base config based on prod/dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/BYODMCSE/",
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+
+  return defineConfig({
+    base: mode === "prod" ? "/BYODMCSE/" : "/",
+    plugins: [react()],
+  });
+}
