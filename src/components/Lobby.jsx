7e955b925258f692a522fafe14d61f34119832b1
Thomas Geissl <thomas.geissl@gmail.com>
2023-05-08 07:04:47 +0200

    added no sleep module

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "@emotion/styled";
 import WebRenderer from "@elemaudio/web-renderer";
+import NoSleep from 'nosleep.js'; 
 import Orchestra from "../audio/Orchestra";
 import Button from "@mui/material/Button";
 import Room from "./Room";
@@ -9,6 +10,7 @@ import Room from "./Room";
 import config from "../assets/config.json"
 
 let core = new WebRenderer();
+var noSleep = new NoSleep();
 
 const loadSample = async (path, ctx) => {
   const res = await fetch(path);
@@ -49,6 +51,7 @@ function Lobby() {
       setOrchestra(orchestra);
       setInited(true);
     });
+    noSleep.enable();
   };
 
   return (
