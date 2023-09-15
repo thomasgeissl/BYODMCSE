2e2df4cb19a8913a02079c8d85ec65f83f6dc678
Thomas Geissl <thomas.geissl@gmail.com>
2023-09-15 23:14:05 +0300

    debugging sampler

--- src/audio/instruments/Sampler.js
+++ src/audio/instruments/Sampler.js
@@ -1,12 +1,5 @@
 import { el } from "@elemaudio/core";
 import { v4 } from "uuid";
-import Lowpass from "../effects/LowPassFilter.js";
-import HighPassFilter from "../effects/HighPassFilter.js";
-
-// https://www.youtube.com/watch?v=0voWrxLDnSE
-
-const lowpass = new Lowpass();
-const highpass = new HighPassFilter();
 
 class Sampler {
   constructor(samples) {
@@ -18,7 +11,7 @@ class Sampler {
         gate: 0.0,
         velocity: 0,
         key: `sampler-v${i}-${v4()}`,
-        sample: null,
+        sampleId: null,
       });
     }
   }
@@ -29,9 +22,9 @@ class Sampler {
       value: voice.gate,
     });
     const env = el.env(4.0, 1.0, 0.4, 2.0, gate);
-    if (voice.sample) {
+    if (voice?.sampleId) {
       const out = el.sample(
-        { path: this.samples[voice.sample].path, mode: "trigger" },
+        { path: this.samples[voice.sampleId].path, mode: "trigger" },
         gate,
         1.0
       );
@@ -43,13 +36,16 @@ class Sampler {
   };
 
   noteOn(note, velocity) {
-    // turn of sample if currently played
-    const oldVoice = this.voices.find((voice) => voice.sample === note);
+    console.log("playing note", note);
+    // turn off sample if currently played
+    const oldVoice = this.voices.find((voice) => voice.sampleId === note);
     if (oldVoice) {
+      console.log("turn off currentlz playing samples");
       oldVoice.gate = 0;
+      oldVoice.velocity = 0;
     }
 
-    let voice = this.voices.find((voice) => voice.sample === null);
+    let voice = this.voices.find((voice) => voice.sampleId === null);
     if (!voice) {
       voice = this.voices.sort((a, b) => a.timestamp - b.timestamp)[
         this.voices.length - 1
@@ -57,7 +53,7 @@ class Sampler {
     }
 
     if (voice) {
-      voice.sample = note;
+      voice.sampleId = note;
       voice.timestamp = new Date();
       voice.gate = 1.0;
       voice.velocity = velocity / 127;
@@ -65,22 +61,23 @@ class Sampler {
   }
   noteOff(note) {
     const voice = this.voices.find((voice) => {
-      return voice.sample == note;
+      return voice.sampleId === note;
     });
     if (voice) {
       voice.gate = 0;
-      voice.sample = null;
+      voice.velocity = 0;
+      voice.sampleId = null;
     }
   }
 
   render() {
-    if (this.voices.filter((voice) => voice.sample != null).length === 0) {
+    if (this.voices.filter((voice) => voice.sampleId !== null).length === 0) {
       // TODO: is there a 0 node?
       return el.mul(el.cycle(440), 0);
     }
     const out = el.add(
       ...this.voices
-        .filter((voice) => voice.sample != null)
+        .filter((voice) => voice.sampleId !== null)
         .map((voice) => {
           return this.voice(voice);
         })
