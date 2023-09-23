cb99a4358b785c579cd784975a7fe7941d6d0f07
Thomas Geissl <thomas.geissl@gmail.com>
2023-09-23 22:07:31 +0200

    removed padding

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -27,7 +27,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  padding: 16px;
+  /* padding: 16px; */
 `;
 const Instructions = styled.div`
   flex-grow: 1;
