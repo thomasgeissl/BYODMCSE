714bc308eea8dee7e34631a8f263e3d57ceeacca
Thomas Geissl <thomas.geissl@gmail.com>
2024-07-06 00:02:43 +0200

    some more tests

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -15,7 +15,7 @@ let ctx;
 
 const core = new WebRenderer();
 const noSleep = new NoSleep();
-let config = staticConfig
+let config = staticConfig;
 
 const loadSample = async (path, ctx) => {
   const res = await fetch(path);
@@ -27,7 +27,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  /* padding: 16px; */
 `;
 const Instructions = styled.div`
   flex-grow: 1;
@@ -47,27 +46,22 @@ function useQuery() {
 function Room() {
   const roomId = useParams().roomId || "taxi";
   const [searchParams] = useSearchParams();
-  if (searchParams.get("config")) {
-    const configPath = searchParams.get("config");
-    axios.get(configPath).then((response) => {
-      console.log(response.data);
-      try {
-        config = JSON.parse(response.data)
-      } catch (error) {
-        
-      }
-      // TODO: should tis be done async?
-      // TODO: set config and load orchestra
-    });
-  }
-
   const [inited, setInited] = useState(false);
   const [loading, setLoading] = useState(false);
   const [orchestra, setOrchestra] = useState(null);
-  if (inited && ctx.state !== "running") {
-    console.log("ctx.state", ctx.state);
-    ctx.resume();
-  }
+
+  useEffect(() => {
+    if (searchParams.get("config")) {
+      const configPath = searchParams.get("config");
+      axios.get(configPath).then((response) => {
+        try {
+          config = JSON.parse(response.data);
+        } catch (error) {
+          console.error("Error parsing config:", error);
+        }
+      });
+    }
+  }, [searchParams]);
 
   const init = async () => {
     setLoading(true);
@@ -76,18 +70,16 @@ function Room() {
       await ctx.resume();
     }
 
-    core.on("meter", function (e) {
+    core.on("meter", (e) => {
       if (e.source === "left") {
         console.log("left peak", e.max);
-        // handleLeftPeakValue(e.max);
       }
       if (e.source === "right") {
         console.log("right peak", e.max);
-        // handleRightPeakValue(e.max);
       }
     });
 
-    core.on("load", async function () {
+    core.on("load", async () => {
       const files = {};
       const entries = Object.entries(config.files);
       for (let i = 0; i < entries.length; i++) {
@@ -96,15 +88,12 @@ function Room() {
       }
 
       core.updateVirtualFileSystem(files);
-      // core.render(el.cycle(440), el.cycle(440));
       const orchestra = new Orchestra(config.orchestra);
       setOrchestra(orchestra);
       setLoading(false);
       setInited(true);
     });
-    // if (ctx.state !== "running") {
-    //   await ctx.resume();
-    // }
+
     const node = await core.initialize(ctx, {
       numberOfInputs: 0,
       numberOfOutputs: 1,
@@ -119,14 +108,10 @@ function Room() {
       {!inited && (
         <>
           <Instructions>
-            please increase your volume to the max and enter the room
+            Please increase your volume to the max and enter the room
           </Instructions>
           <Button
-            onClick={async () => {
-              // setTimeout(() => {
-              await init();
-              // }, 200);
-            }}
+            onClick={init}
             variant={"outlined"}
             size="large"
             sx={{
@@ -153,16 +138,12 @@ function Room() {
                 }}
               />
             )}
-            enter
+            Enter
           </Button>
         </>
       )}
       {inited && (
-        <Stage
-          core={core}
-          orchestra={orchestra}
-          mappings={config.mappings}
-        ></Stage>
+        <Stage core={core} orchestra={orchestra} mappings={config.mappings} />
       )}
     </Container>
   );
